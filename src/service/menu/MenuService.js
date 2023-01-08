c83cecc41c9a456e255e2c8b9a5a3e60c74be9bb
minseok <minseok9128@gmail.com>
2023-01-08 16:42:07 +0900

    deploy test

--- src/service/menu/MenuService.js
+++ src/service/menu/MenuService.js
@@ -4,7 +4,7 @@ const MenuService = {
   sayMenus: async ({ message, say }) => {
     const contents = new SlackBlockBuilder()
       .addTitle('title')
-      .addButton('TEST2', 'test2')
+      .addButton('TEST1', 'test1')
       .addDivider()
       .addTitle('title2')
       .addButton('TEST2', 'test2')
